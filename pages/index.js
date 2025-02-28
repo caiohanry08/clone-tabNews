8bc3c9749e9bc5cdf8a29f59e102db4638808775
caiohanry08 <123168365+caiohanry08@users.noreply.github.com>
2025-02-28 22:02:59 +0000

    configure prettier on project

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home () {
-    return <h1> Site under Construction!</h1>
+function Home() {
+  return <h1> Site under Construction!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
